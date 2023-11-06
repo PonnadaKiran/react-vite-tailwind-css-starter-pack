a49b88dba90e4d72527cb2778862cb9703d92487
PonnadaKiran <kiranponnada28@gmail.com>
2023-11-06 11:21:18 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div className="bg-blue-500 text-white p-4">
+      This is the React + Vite + TailwindCSS Starter Pack!
+    </div>
+  )
+}
+
+export default App
